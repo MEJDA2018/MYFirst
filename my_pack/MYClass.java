92cb3411847c7c2b006f62e023b8b306a6770d1c
MEJDA2018 <ahia2011ahia@gmail.com>
2019-07-14 09:23:10 +0000

    first add

---
+++ my_pack/MYClass.java
@@ -0,0 +1,11 @@
+package my_pack;
+
+/**
+ *  Created by ProgrammingKnowledge on 20.03.15
+ */
+public class MYClass {
+    public static void main(String[] args) {
+        System.out.println("Salam Alaykoummm !!!");
+    }
+
+}
